557937b9e4788aa05d3400463a50cc0ddff5ec4c
Viiiiice <minifanyin676@gmail.com>
2026-06-23 17:13:14 -0500

    upgrade 4

--- loader.lua
+++ loader.lua
@@ -1,5 +1,6 @@
 local gameId = game.GameId
 if gameId == 2309918273 or gameId == 10129349136 then
+    
     pcall(function()
         loadstring(game:HttpGet(
             "https://raw.githubusercontent.com/Viiiiice/vvv2/main/MAIN/runner.lua",
