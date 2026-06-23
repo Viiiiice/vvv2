828c387e451730724ca0ddc47f63afed6dd81407
Viiiiice <minifanyin676@gmail.com>
2026-06-23 16:46:41 -0500

    Track empty folders

---
+++ loader.lua
@@ -0,0 +1,9 @@
+local gameId = game.GameId
+if gameId == 2309918273 then
+    pcall(function()
+        loadstring(game:HttpGet(
+            "https://raw.githubusercontent.com/Viiiiice/vvv/MAIN/runner.lua",
+            true
+        ))()
+    end)
+end
