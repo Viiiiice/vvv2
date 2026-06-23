5d74ea8842d7072f255b89a7d0fa2f4d38f2eaab
Viiiiice <minifanyin676@gmail.com>
2026-06-23 17:30:54 -0500

    ttttttasdfasdfasdfzxcvzxv

--- loader.lua
+++ loader.lua
@@ -1,12 +1,9 @@
 local gameId = game.GameId
-if gameId == 2309918273 or gameId == 10129349136 then
-    
-    local success, err = pcall(function()
+if gameId == 10129349136 then
+        pcall(function()
         loadstring(game:HttpGet(
             "https://raw.githubusercontent.com/Viiiiice/vvv2/main/MAIN/runner.lua",
             true
         ))()
     end)
-
-    print(success, err)
 end
